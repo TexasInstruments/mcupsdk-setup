7ca1416ce6ce83ac87a2f8ef94dfe3e27e00598a
Venkatesan Krishnamoorthy <v-krishnamoorthy@ti.com>
2024-08-23 15:20:30 +0530

    am65x: add 9.3.0
     - add 9.3.0 for am65x
    
    Signed-off-by: Venkatesan Krishnamoorthy <v-krishnamoorthy@ti.com>

--- am65x/download_components.sh
+++ am65x/download_components.sh
@@ -50,7 +50,7 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 : ${skip_doxygen:="false"}
 : ${skip_ccs:="false"}
 
-release_version=09_02_01
+release_version=09_03_00
 product_family="am65x"
 THIS_DIR=$(dirname $(realpath $0))
 BASE_DIR=$(realpath ${THIS_DIR}/..)
