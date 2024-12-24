ffb2f7734058c0a28bce72dcec8cd497a182a1cb
Venkatesan Krishnamoorthy <v-krishnamoorthy@ti.com>
2024-12-24 11:50:37 +0530

    all: release: add directory for release 11_00_00
      -  add directory for release 11_00_0
    
    Signed-off-by: Venkatesan Krishnamoorthy <v-krishnamoorthy@ti.com>

--- am62x/download_components.sh
+++ am62x/download_components.sh
@@ -56,7 +56,7 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 : ${skip_ccs:="false"}
 : ${skip_dtc:="false"}
 
-release_version=10_01_00
+release_version=11_00_00
 product_family="am62x"
 THIS_DIR=$(dirname $(realpath $0))
 BASE_DIR=$(realpath ${THIS_DIR}/..)
