a36fcefa4f2ea6e6422a80e8dc7a470ec2bc172b
Venkatesan Krishnamoorthy <v-krishnamoorthy@ti.com>
2025-03-06 08:37:24 +0530

    am243x: script: add download component script for 11.00.00
       - add 11_00_00 download scripts for am243x
    
    Fixes: SITSW-6685
    
    Signed-off-by: Venkatesan Krishnamoorthy <v-krishnamoorthy@ti.com>

--- am243x/download_components.sh
+++ am243x/download_components.sh
@@ -49,7 +49,7 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 : ${skip_doxygen:="false"}
 : ${skip_ccs:="false"}
 
-release_version=10_01_00
+release_version=11_00_00
 product_family="am243x"
 THIS_DIR=$(dirname $(realpath $0))
 BASE_DIR=$(realpath ${THIS_DIR}/..)
